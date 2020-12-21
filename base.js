69d352232922f14d2b4e93cc3f710da477f203e7
Soybeanman504 <70052390+Soybeanman504@users.noreply.github.com>
2020-12-21 17:38:02 +0900

    Add files via upload

---
+++ base.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+// 1.各々のディレクトリのindexが親indexに飛ばすようにする
+// 2.「./index.hrml#ディレクトリ名」飛ばし、「#」消去後、history.replaceState()でURL書き換え
+
+// または普通に親indexを子に複製後挙動だけ確認そのまま使用(相対パスなのでほぼ変えずに流用可のはず)
