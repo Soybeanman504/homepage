0b07d7f3ed19540af0c0b2f3575c378daad533f9
Soybeanman504 <70052390+Soybeanman504@users.noreply.github.com>
2020-12-23 17:19:48 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,27 +7,29 @@ var path = queries.path;
 
 history.replaceState('','',path);
 
-var iframe = document.getElementById('mainframe');
+window.onload = function(){
+  var iframe = document.getElementById('mainframe');
 
-console.log(iframe);
-iframe.src = path + 'main.html';
+  console.log(iframe);
+  iframe.src = path + 'main.html';
+}
 
 function getUrlQueries() {
-    var queryStr = window.location.search.slice(1);  // 文頭?を除外
-        queries = {};
+  var queryStr = window.location.search.slice(1);  // 文頭?を除外
+  queries = {};
   
-    // クエリがない場合は空のオブジェクトを返す
-    if (!queryStr) {
-      return queries;
-    }
-  
-    // クエリ文字列を & で分割して処理
-    queryStr.split('&').forEach(function(queryStr) {
-      // = で分割してkey,valueをオブジェクトに格納
-      var queryArr = queryStr.split('=');
-      queries[queryArr[0]] = queryArr[1];
-    });
-  
+  // クエリがない場合は空のオブジェクトを返す
+  if (!queryStr) {
     return queries;
   }
+
+  // クエリ文字列を & で分割して処理
+  queryStr.split('&').forEach(function(queryStr) {
+    // = で分割してkey,valueをオブジェクトに格納
+    var queryArr = queryStr.split('=');
+    queries[queryArr[0]] = queryArr[1];
+  });
+  
+  return queries;
+}
   
