28a5214d5b868bda58729dfb31b261640f249653
Soybeanman504 <70052390+Soybeanman504@users.noreply.github.com>
2020-12-25 21:11:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ window.onload = function(){
   // リダイレクト後
   var queries = getUrlQueries();
   var path = queries.path;
-  console.log(path);
+  
   if (path) {
     iframeChange('mainframe', path);
   } else {
@@ -15,12 +15,13 @@ window.onload = function(){
   }
 
   // iframeの変更に応じて色々変える
+  var iframe = document.getElementById('mainframe');
+  
   Object.defineProperty(iframe, 'src', {get: () => value,
     set: newValue => {
       const oldValue = value;
       value = newValue;
       
-      let iframe = document.getElementById('mainframe');
       let path = iframe.src;
       iframeChange('mainframe', path.slice(0, path.lastIndexOf('/') + 1));
     },
@@ -30,7 +31,6 @@ window.onload = function(){
   // リンク踏んだ時の対応
   var clickEventType = ((window.ontouchstart!==null) ? 'click':'touchend');
   var links = document.getElementsByClassName('link');
-  console.log(links);
 
   for(let n = 0; n < links.length; n++){
     let link = links[n];
