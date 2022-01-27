4fb2bb0bc32c9317a1a58f0340dbbc43210b2d87
Soybeanman504 <70052390+Soybeanman504@users.noreply.github.com>
2022-01-27 16:24:47 +0900

    add img

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Col from 'react-bootstrap/Col';
 
 class App extends React.Component {
   render() {
-    let colLefts = ["alice.png","flandre scarlet.gif","kirisame marisa3.png","kirisame marisa4.png","maguro.png","Rumia.png","sheep.png","test.png","yorigami_shion.gif"];
-    let colRights = ["camera.gif","city.png","hinanawi-tenshi-B.gif","koishi2.png","marisa.png","sea-chicken.png","tamatsukuri_misumaru.png","yamame2.png","yuuka2.png"];
+    let colLefts = ["alice.png","flandre scarlet.gif","hinanawi-tenshi-B.gif","kirisame marisa4.png","maguro.png","Rumia.png","sheep.png","tamatsukuri_misumaru.png","test.png","yamame2.png","yuuka2.png"];
+    let colRight = ["camera.gif","city.png","hakurei_reimu.png","kirisame marisa3.png","koishi2.png","marisa.png","sea-chicken.png","small-raimu.png","tenshi.png","yorigami_shion.gif","zourimushi.png"];
 
     /*
     for (let n = 0; n < iconNames.length; n++) {
